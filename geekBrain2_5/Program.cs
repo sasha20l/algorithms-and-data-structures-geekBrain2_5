fd46b7e3d4459207de6eef46f0bf1d5869562d58
Александр Марфицын <nonesasha225@gmail.com>
2021-02-05 21:39:28 +0500

    Изменение 1

--- geekBrain2_5/Program.cs
+++ geekBrain2_5/Program.cs
@@ -10,31 +10,18 @@ namespace geekBrain2_5
     {
         static void Main(string[] args)
         {
-            Node<int> node = Insert(new Node<int>(), 100);
-            dff(node, 10);
-            bff(node, 10);
-            Console.ReadLine();
-        }
-
-        static Node<int> GetFreeNode(int value, Node<int> head)
-        {
-            //Node<int> newNode = null;
-            //if (value == 0)
-            //    return null;
-            //else
-            //{
-            //    var nl = value / 2;
-            //    var nr = value - nl - 1;
-            //    newNode = new Node<int>();
-            //    newNode.Data = new Random().Next();
-            //    newNode.Left = GetFreeNode(nl, newNode);
-            //    newNode.Right = GetFreeNode(nr, newNode);
-            //}
-            //return newNode;
             Node<int> node = new Node<int>();
-            node.Data = value-10;
-            node.Parent = head;
-            return node;
+            Insert(node, 10);
+            Insert(node, 12);
+            Insert(node, 8);
+            Insert(node, 9);
+            Insert(node, 7);
+            Insert(node, 3);
+            Insert(node, 4);
+            dff(node, 9);
+            bff(node, 9);
+
+            Console.ReadLine();
         }
 
         public static bool dff(Node<int> node, int value)
@@ -45,7 +32,7 @@ namespace geekBrain2_5
             if (node.Data == value)
             {
                 Console.WriteLine($"Находимся в узле {node.Data}, узел СОВПАЛ с искомым значением {value}");
-                return true;    
+                return true;
             }
             else if (node.visited) return false;
             Console.WriteLine($"Находимся в узле {node.Data}, узел НЕ совпал с искомым значением {value}");
@@ -80,10 +67,10 @@ namespace geekBrain2_5
                     {
                         stack.Push(nod);
                         nod.visited = true;
-                        if (nod.Data == value) 
+                        if (nod.Data == value)
                         {
                             Console.WriteLine($"Находимся в узле {nod.Data}, узел СОВПАЛ с искомым значением {value}");
-                            return true; 
+                            return true;
                         }
                     }
                     Console.WriteLine($"Находимся в узле {nod.Data}, узел НЕ совпал с искомым значением {value}");
@@ -92,14 +79,43 @@ namespace geekBrain2_5
             return false;
         }
 
+        static void deleteValue(Node<int> node, int value)
+        {
+            Node<int> target = getNodeByValue(node, value);
+            removeNodeByPtr(target);
+        }
+
+        static void printTree(Node<int> node, string str, int level)
+        {
+            if (node != null)
+            {
+
+                Console.WriteLine($"{level} {str} {node.Data}");
+                printTree(node.Left, "left", level + 1);
+                printTree(node.Right, "right", level + 1);
+            }
+        }
+
+        static Node<int> GetFreeNode(int value, Node<int> head)
+        {
+            Node<int> tmp = new Node<int>();
+            tmp.Left = null;
+            tmp.Right = null;
+            tmp.Data = value;
+            tmp.Parent = head;
+            return tmp;
+
+        }
         public static Node<int> Insert(Node<int> head, int value)
         {
             Node<int> tmp = null;
+
             if (head == null)
             {
                 head = GetFreeNode(value, null);
                 return head;
             }
+
             tmp = head;
             while (tmp != null)
             {
@@ -112,7 +128,7 @@ namespace geekBrain2_5
                     }
                     else
                     {
-                        tmp.Right = GetFreeNode(value, tmp); ;
+                        tmp.Right = GetFreeNode(value, tmp);
                         return head;
                     }
                 }
@@ -131,11 +147,93 @@ namespace geekBrain2_5
                 }
                 else
                 {
-                    throw new Exception("Wrong tree state");                  // Дерево построено неправильно
+                    throw new Exception("Wrong tree state");                  // ƒерево построено неправильно
                 }
             }
-            return tmp;
+            return head;
         }
+        public static Node<int> getMinNode(Node<int> node)
+        {
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node;
+        }
+
+        public static Node<int> getMaxNode(Node<int> node)
+        {
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+            return node;
+        }
+
+        public static Node<int> getNodeByValue(Node<int> node, int value)
+        {
+            while (node != null)
+            {
+                if (node.Data > value)
+                {
+                    node = node.Left;
+                    continue;
+                }
+                else if (node.Data < value)
+                {
+                    node = node.Right;
+                    continue;
+                }
+                else return node;
+            }
+            return null;
+        }
+
+        public static void removeNodeByPtr(Node<int> node)
+        {
+            if (node.Left != null && node.Right != null)
+            {
+                Node<int> localMax = getMaxNode(node.Left);
+                node.Data = localMax.Data;
+                removeNodeByPtr(localMax);
+                return;
+            }
+            else if (node.Left != null)
+            {
+                if (node == node.Parent.Left)
+                {
+                    node.Parent.Left = node.Left;
+                }
+                else
+                {
+                    node.Parent.Right = node.Left;
+                }
+            }
+            else if (node.Right != null)
+            {
+                if (node == node.Parent.Right)
+                {
+                    node.Parent.Right = node.Right;
+                }
+                else
+                {
+                    node.Parent.Left = node.Right;
+                }
+            }
+            else
+            {
+                if (node == node.Parent.Left)
+                {
+                    node.Parent.Left = null;
+                }
+                else
+                {
+                    node.Parent.Right = null;
+                }
+            }
+            return;
+        }
+
     }
 
     public class Node<T>
